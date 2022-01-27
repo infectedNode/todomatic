00b69393475449324e2c4de6a50eecb15c582471
Shivam Sharma <shivam231198@gmail.com>
2022-01-27 16:15:03 +0530

    done with react interactivity: events and state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+import { nanoid } from "nanoid";
+
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
 function App(props) {
-  const taskList = props.tasks.map(task => (
+
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const addTask = (name) => {
+    const newTask = { id: "todo-" + nanoid(), name, completed: false};
+    setTasks([...tasks, newTask]);
+  }
+
+  const toggleTaskCompleted = (id) => {
+    const updatedTasks = tasks.map(task => {
+      if (id === task.id) {
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  
+  const deleteTask = (id) => {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }  
+
+  const taskList = tasks?.map(task => (
     <Todo
         id={task.id}
         name={task.name}
         completed={task.completed}
         key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
       />
     )
   );
+
+  const headingText = `${taskList.length} tasks remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
